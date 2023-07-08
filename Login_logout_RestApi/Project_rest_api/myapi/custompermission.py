2aef6ef4a77dbd3ba71fd74d156a92b40087d88a
Ram sharma <102453607+sharmaram10298@users.noreply.github.com>
2023-07-08 21:41:23 +0530

    Update custompermission.py

--- Login_logout_RestApi/Project_rest_api/myapi/custompermission.py
+++ Login_logout_RestApi/Project_rest_api/myapi/custompermission.py
@@ -1,9 +1,1 @@
-from rest_framework.permissions import BasePermission
 
-
-
-class EmployeePermission(BasePermission):
-    def emp_permission(self, request, view):
-        if request.method == 'GET':
-            return True
-        return False
